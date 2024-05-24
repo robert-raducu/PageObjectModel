cb90c7f70b031fc8ef4d20b9e61a620825cec1b0
Robert <robert_raducu@yahoo.com>
2024-05-24 16:07:07 +0300

    curs 24

---
+++ src/main/java/selenium/utils/BaseTest.java
@@ -0,0 +1,34 @@
+package selenium.utils;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+
+	public WebDriver driver;
+	
+	@BeforeClass
+	public void setup() {
+		// TestNG e ft similara cu JUnit
+		// Ne ofera functionalitati suplimentare (TestNG)
+		// JUnit - teste unitare
+		
+		//System.set.property("debdriver.chrome.driver"."path to chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://keybooks.ro/"); //ii permite sa navigheze catre aplicatie 
+	}
+	
+	
+	@AfterClass
+	public void tearDown() throws InterruptedException {
+		Thread.sleep(5000); // bad practice, ar trebui sa apuc si eu sa vad ceva 
+		//driver.close(); //inchide tabul curent
+		driver.quit(); // inchide fereastra de browser
+	}
+}
